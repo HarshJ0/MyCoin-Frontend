ad88261dc6426b3f9fddbc31d512aa24f9e95d0b
HarshJ0 <91450804+HarshJ0@users.noreply.github.com>
2022-02-25 15:00:24 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { BlockchainService } from './services/blockchain.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  public blockchain: any;
+
+  constructor(private blockchainService: BlockchainService) {
+    this.blockchain = blockchainService.blockchainInstance;
+  }
+
+  ngOnInit(): void {
+  }
+
+  thereArePendingTransactions() {
+    return this.blockchain.pendingTransactions.length > 0;
+  }
+
+}
